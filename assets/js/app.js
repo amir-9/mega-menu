e54af3cbc7ed82ca1c104b8ee4c8950da3b87dd8
AmirHosseinHimSelf <amirr.969492@gmail.com>
2022-01-31 13:58:26 +0330

    fixed navbar first item hover

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 let $el, leftPos, newWidth;
-
+var cnt;
 /* Add Magic Line markup via JavaScript, because it ain't going to work without */
 $("#example-one").append("<li id='magic-line'></li>");
 
@@ -11,40 +11,65 @@ $magicLine
     .data("origLeft", $magicLine.position().left)
     .data("origWidth", $magicLine.width());
 
+console.log($("#example-one>li"))
+
+$(".current_page_item").hover(
+    function () {
+        cnt = 1;
+
+    }, function () {
+        cnt=0
+    }
+
+)
+
 $("#example-one>li")
     .find("a")
     .hover(
         function () {
-            $el = $(this);
-            leftPos = $el.position().left;
-            newWidth = $el.parent().width();
-            // console.log(leftPos)
-            $magicLine.stop().animate({
-                left: leftPos,
-                width: newWidth,
-                opacity: 1
-            });
+            if(cnt===0) {
+                $el = $(this);
+                leftPos = $el.position().left - 8;
+                // $magicLine.css("left","818.5225px !important")
+                newWidth = $el.parent().width();
+                // console.log(leftPos)
+                $magicLine.stop().animate({
+                    left: leftPos,
+                    width: newWidth,
+                    opacity: 1
+                });
+            }else{
+                $el = $(this);
+                leftPos = '820px';
+
+                newWidth = $el.parent().width();
+                $magicLine.stop().animate({
+                    left: leftPos,
+                    width: newWidth,
+                    opacity: 1
+                });
+            }
         },
         function () {
             $magicLine.stop().animate({
                 width: 0,
-                opacity:0
+                opacity: 0
             });
         }
     );
 
-$magicLine[0].style.width='0'
+$magicLine[0].style.width = '0'
 ////////
-let section=$('section')
-let currentPageItem=$('.current_page_item');
-let megaMenu=$('.mega-box');
+let section = $('section')
+let currentPageItem = $('.current_page_item');
+let megaMenu = $('.mega-box');
 megaMenu.fadeOut(0)
 currentPageItem.hover(
     function () {
         megaMenu.fadeIn(200)
         section.toggleClass('section-darken')
     },
-    function (){
+    function () {
         megaMenu.fadeOut(200)
         section.toggleClass('section-darken')
     }
